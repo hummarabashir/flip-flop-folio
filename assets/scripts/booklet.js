7b782b73eac6dee52c2dbd3121efac4b517c5884
Hummara Bashir <hummarabashir@gmail.com>
2023-03-01 07:47:52 -0500

    favicon

--- assets/scripts/booklet.js
+++ assets/scripts/booklet.js
@@ -1,45 +1,7 @@
-// function foldSignature(foldDuration) {
-//   $("#p_4, #p_5, #p_12, #p_13").css({
-//       "-webkit-animation-name": "fold1",
-//       "animation-name": "fold1",
-//       "-webkit-animation-fill-mode": "forwards",
-//       "animation-fill-mode": "forwards",
-//       "-webkit-animation-delay": "",
-//       "animation-delay": "",
-//       "-webkit-animation-duration": foldDuration / 3 + "s",
-//       "animation-duration": foldDuration / 3 + "s",});
-//   $("#p_8, #p_9").css({
-//       "-webkit-transform-origin": "bottom",
-//       "transform-origin": "bottom",
-//       "-webkit-animation-name": "fold2",
-//       "animation-name": "fold2",
-//       "-webkit-animation-fill-mode": "forwards",
-//       "animation-fill-mode": "forwards",
-//       "-webkit-animation-delay": foldDuration / 3 + "s",
-//       "animation-delay": foldDuration / 3 + "s",
-//       "-webkit-animation-duration": foldDuration / 3 + "s",
-//       "animation-duration": foldDuration / 3 + "s",});
-//   $("#p_16").css({
-//       "-webkit-transform-origin": "right",
-//       "transform-origin": "right",
-//       "-webkit-animation-name": "fold3",
-//       "animation-name": "fold3",
-//       "-webkit-animation-delay": (foldDuration / 3) * 2 + "s",
-//       "animation-delay": (foldDuration / 3) * 2 + "s",
-//       "-webkit-animation-duration": foldDuration / 3 + "s",
-//       "animation-duration": foldDuration / 3 + "s",});
-// }
-
 function defaultfoldSignature(foldDuration) {
   $("#p_4, #p_5, #p_12, #p_13").css({
       "-webkit-animation-name": "fold1",
       "animation-name": "fold1",
-      // "-webkit-animation-fill-mode": "forwards",
-      // "animation-fill-mode": "forwards",
-      // "-webkit-animation-delay": "",
-      // "animation-delay": "",
-      // "-webkit-animation-duration": foldDuration / 3 + "s",
-      // "animation-duration": foldDuration / 3 + "s",
     });
   $("#p_16, #p_1").css({
       "-webkit-transform-origin": "top",
@@ -48,20 +10,10 @@ function defaultfoldSignature(foldDuration) {
       "animation-name": "fold2",
       "-webkit-animation-fill-mode": "forwards",
       "animation-fill-mode": "forwards",
-      // "-webkit-animation-delay": foldDuration / 3 + "s",
-      // "animation-delay": foldDuration / 3 + "s",
-      // "-webkit-animation-duration": foldDuration / 3 + "s",
-      // "animation-duration": foldDuration / 3 + "s",
     });
   $("#p_8").css({
-      // "-webkit-transform-origin": "left",
-      // "transform-origin": "left",
       "-webkit-animation-name": "fold3",
       "animation-name": "fold3",
-      // "-webkit-animation-delay": (foldDuration / 3) * 2 + "s",
-      // "animation-delay": (foldDuration / 3) * 2 + "s",
-      // "-webkit-animation-duration": foldDuration / 3 + "s",
-      // "animation-duration": foldDuration / 3 + "s",
     });
 }
 function foldSignature(foldDuration) {
@@ -130,38 +82,6 @@ function unfoldSignature(foldDuration) {
       "animation-duration": foldDuration / 3 + "s",});
 }
 
-// function unfoldSignature(foldDuration) {
-//   $("#p_16").css({
-//       "-webkit-transform-origin": "right",
-//       "transform-origin": "right",
-//       "-webkit-animation-name": "unfold3",
-//       "animation-name": "unfold3",
-//       "-webkit-animation-delay": "",
-//       "animation-delay": "",
-//       "-webkit-animation-duration": foldDuration / 3 + "s",
-//       "animation-duration": foldDuration / 3 + "s",});
-//   $("#p_8, #p_9").css({
-//       "-webkit-transform-origin": "bottom",
-//       "transform-origin": "bottom",
-//       "-webkit-animation-name": "unfold2",
-//       "animation-name": "unfold2",
-//       "-webkit-animation-fill-mode": "backwards",
-//       "animation-fill-mode": "backwards",
-//       "-webkit-animation-delay": foldDuration / 3 + "s",
-//       "animation-delay": foldDuration / 3 + "s",
-//       "-webkit-animation-duration": foldDuration / 3 + "s",
-//       "animation-duration": foldDuration / 3 + "s",});
-//   $("#p_4, #p_5, #p_12, #p_13").css({
-//       "-webkit-animation-name": "unfold1",
-//       "animation-name": "unfold1",
-//       "-webkit-animation-fill-mode": "backwards",
-//       "animation-fill-mode": "backwards",
-//       "-webkit-animation-delay": (foldDuration / 3) * 2 + "s",
-//       "animation-delay": (foldDuration / 3) * 2 + "s",
-//       "-webkit-animation-duration": foldDuration / 3 + "s",
-//       "animation-duration": foldDuration / 3 + "s",});
-// }
-
 function flipSignature(flipDuration) {
   $(".page-wrapper.front").css({
     "-webkit-animation-name": "flipB",
@@ -187,9 +107,6 @@ function flipSignatureBack(flipDuration) {
     "-webkit-animation-duration": flipDuration + "s",
     "animation-duration": flipDuration + "s",});
 }
-
-
-
 
 $("#turn").click(function() {
   if ( $(".page-wrapper").hasClass("folded") ) {
@@ -253,6 +170,7 @@ function flipFlip(flipDuration) {
 
 $("#flip").click(function() {
   if ( $(".page-wrapper").hasClass("folded") ) {
+    // $("#flip").addClass("disabled");
     unfoldSignature(1);
     foldAction = 1;
     $(".page-wrapper").removeClass("folded");
@@ -260,4 +178,8 @@ $("#flip").click(function() {
   } else {
     flipFlip(1);
   }
+});
+
+$("#print").click(function(){
+  window.print();
 });
